16f10b07f574775904d33793565ed4b9ac280806
David Herrmann <davherrmann@googlemail.com>
2020-03-21 00:30:11 +0100

    implement bot using telegraf & now functions

---
+++ api/bot/[token].ts
@@ -0,0 +1,23 @@
+import Telegraf from "telegraf"
+import { NowRequest, NowResponse } from "@now/node"
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
+bot.on("text", ({ reply }) =>
+  reply(
+    "Hallo, ich bin der freundliche Bot vom Mobilen Arztzimmer. Ich freue mich, wenn ich Dir helfen kann. ❤️",
+  ),
+)
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const {
+    query: { token },
+  } = req
+
+  // return if secret url does not match
+  if (token !== process.env.TELEGRAM_SECRET_URL) {
+    res.status(404).send("")
+    return
+  }
+
+  await bot.handleUpdate(req.body, res)
+}
