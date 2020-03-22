c6034626c53f2174767924616fc2dff836b8e012
David Herrmann <davherrmann@googlemail.com>
2020-03-22 15:34:27 +0100

    Dialog bis zum Warten auf die Fachkraft

--- pages/api/bot/[token].ts
+++ pages/api/bot/[token].ts
@@ -12,13 +12,19 @@ interface ContextWithSession extends ContextMessageUpdate {
 
 enum State {
   NutzerUnbekannt,
-  HatNutzerAlleVierCoronaSymptome,
+  HatPatientSymptome,
   NutzerIstArzt,
-  NutzerHatAlleVierCoronaSymptome,
+  KenntPatientCovid19Erkrankte,
   NutzerHatNichtAlleVierCoronaSymptome,
-  HilftErholungUndTrinken,
-  NutzerWurdeGeholfen,
+  WarPatientNahBeiCovid19Erkranktem,
+  HattePatientDirektenKontakt,
   KontaktAufnehmen,
+  HattePatientKontaktMitKoerperFluessigkeit,
+  HattePatientSitzplatzInNaehe,
+  HattePatientKontaktMitGesundheitsamt,
+  NutzerSendetKontakt,
+  NutzerSendetStandort,
+  NutzerWartetAufTerminbestätigung,
 }
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN || "")
@@ -31,8 +37,8 @@ bot.use(async (ctx, next: any) => {
 })
 
 // Prüfungen des Zustandes & Übergang
-const hatNutzerAlleVierCoronaSymptome = (ctx: ContextWithSession) =>
-  ctx.session.state === State.HatNutzerAlleVierCoronaSymptome &&
+const hatNutzerSymptome = (ctx: ContextWithSession) =>
+  ctx.session.state === State.HatPatientSymptome &&
   ctx.callbackQuery?.data === "ja"
 
 // Aktionen
@@ -95,32 +101,95 @@ bot.on("callback_query", async ctxWithoutSession => {
   const { reply, answerCbQuery, session } = ctx
   answerCbQuery()
 
-  if (hatNutzerAlleVierCoronaSymptome(ctx)) {
-    session.state = State.NutzerHatAlleVierCoronaSymptome
-    frageNutzerNachWeiterenRisikoMerkmalen(ctx)
+  if (entsprichtAntwort(ctx, State.HatPatientSymptome, "ja")) {
+    session.state = State.KenntPatientCovid19Erkrankte
+    await stelleJaNeinFrage(
+      ctx,
+      "Kennst du jemanden, die oder der nachweislich Covid-19 🦠hat oder hatte?",
+    )
     return
   }
 
-  if (entsprichtAntwort(ctx, State.NutzerHatAlleVierCoronaSymptome, "nichts")) {
-    session.state = State.HilftErholungUndTrinken
-    await stelleJaNeinFrage(ctx, "Hilft viel Trinken und Erholung?")
+  if (entsprichtAntwort(ctx, State.KenntPatientCovid19Erkrankte, "ja")) {
+    session.state = State.WarPatientNahBeiCovid19Erkranktem
+    await stelleJaNeinFrage(
+      ctx,
+      "Hast du dich in der Zeit, in der die Person krank war oder maximal zwei Tage vor Beginn der ersten Symptome (insbesondere Fieber und Husten) in deiner Nähe befunden?",
+    )
     return
   }
 
-  if (entsprichtAntwort(ctx, State.HilftErholungUndTrinken, "ja")) {
-    session.state = State.NutzerWurdeGeholfen
-    await reply("Kein Grund zur Sorge. 🎉")
+  if (entsprichtAntwort(ctx, State.WarPatientNahBeiCovid19Erkranktem, "ja")) {
+    session.state = State.HattePatientDirektenKontakt
+    await stelleJaNeinFrage(
+      ctx,
+      "Hattest du mindestens 15 Minuten direkten Kontakt zu der Person, etwa in einem persönlichen Gespräch?",
+    )
+    return
+  }
+
+  if (entsprichtAntwort(ctx, State.HattePatientDirektenKontakt, "nein")) {
+    session.state = State.HattePatientKontaktMitKoerperFluessigkeit
+    await stelleJaNeinFrage(
+      ctx,
+      "Hast du eine Körperflüssigkeit der Person berührt, etwa durch Küssen, Anniesen oder Husten?",
+    )
+    return
   }
 
   if (
-    entsprichtAntwort(ctx, State.HilftErholungUndTrinken, "nein") ||
-    entsprichtAntwortNicht(ctx, State.NutzerHatAlleVierCoronaSymptome, "nichts")
+    entsprichtAntwort(
+      ctx,
+      State.HattePatientKontaktMitKoerperFluessigkeit,
+      "nein",
+    )
+  ) {
+    session.state = State.HattePatientSitzplatzInNaehe
+    await stelleJaNeinFrage(
+      ctx,
+      "Hattest du einen Sitzplatz in einem Zug oder Flugzeug zwei Reihen vor, hinter oder in derselben Reihe wie die Person?",
+    )
+    return
+  }
+
+  if (entsprichtAntwort(ctx, State.HattePatientSitzplatzInNaehe, "ja")) {
+    session.state = State.HattePatientKontaktMitGesundheitsamt
+    ;(ctx as any).webhookReply = false
+    await reply(
+      "⚠️ Du wirst als Kontaktperson Kategorie I höheres Infektionsrisiko eingestuft.",
+    )
+    ;(ctx as any).webhookReply = true
+    await sleep(1500)
+    await stelleJaNeinFrage(
+      ctx,
+      "Hast du bereits mit dem Gesundheitsamt Kontakt aufgenommen, oder hat dich das Gesundheitsamt kontaktiert? ",
+    )
+    return
+  }
+
+  if (
+    entsprichtAntwort(ctx, State.HattePatientKontaktMitGesundheitsamt, "nein")
+  ) {
+    session.state = State.NutzerSendetKontakt
+    ;(ctx as any).webhookReply = false
+    await reply(
+      "Ok, kein Problem! Wir übernehmen das für dich und veranlassen, dass ein mobiles Testteam(Doctor Icon/ oder ähnlich) zu dir kommt, um einen Covid-19 Test duchzuführen.",
+    )
+    ;(ctx as any).webhookReply = true
+    await reply("Sag uns doch bitte, wie du mit Vor- und Nachname heißt.")
+    return
+  }
+
+  if (
+    entsprichtAntwort(ctx, State.WarPatientNahBeiCovid19Erkranktem, "nein") ||
+    entsprichtAntwortNicht(ctx, State.KenntPatientCovid19Erkrankte, "nichts")
   ) {
     session.state = State.KontaktAufnehmen
     await reply("Bitte nehmen Sie Kontakt auf. 🆘")
+    return
   }
 
-  if (entsprichtAntwort(ctx, State.HatNutzerAlleVierCoronaSymptome, "nein")) {
+  if (entsprichtAntwort(ctx, State.HatPatientSymptome, "nein")) {
     session.state = State.NutzerHatNichtAlleVierCoronaSymptome
     await reply(
       "Super, dann kann ich Dir vielleicht ein anderes Mal weiterhelfen.",
@@ -129,6 +198,10 @@ bot.on("callback_query", async ctxWithoutSession => {
   }
 })
 
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 bot.on("message", async ctxWithoutSession => {
   const ctx = ctxWithoutSession as ContextWithSession
   const { reply, message, session } = ctx
@@ -136,17 +209,43 @@ bot.on("message", async ctxWithoutSession => {
   session.state = session.state || State.NutzerUnbekannt
 
   if (text?.startsWith("/ichbinpatient")) {
-    session.state = State.HatNutzerAlleVierCoronaSymptome
-    await stelleJaNeinFrage(ctx, "Hast Du alle vier Corona-Symptome?")
+    // TODO in Methode auslagern
+    ;(ctx as any).webhookReply = false
+    await reply(
+      "Guten Tag! Du möchtest wissen ob du dich mit dem Sars-CoV-2 Virus infiziert hast? Dann beantworte bitte folgende Fragen. Sollte sich ein Verdacht ergeben, können wir dir medizinische Hilfe anbieten",
+    )
+    await sleep(1000)
+    ;(ctx as any).webhookReply = true
+    session.state = State.HatPatientSymptome
+    await stelleJaNeinFrage(
+      ctx,
+      "Hast du Symptome wie Fieber 🌡 oder Husten 😷?",
+    )
+    return
+  }
+
+  if (session.state === State.NutzerSendetKontakt) {
+    session.state = State.NutzerSendetStandort
+    await reply(
+      "Vielen Dank! Als nächstes benötigen wir deine Adresse 🏠, bitte übermittle uns deinen Standort (Tippe auf 📎 und wähle Standort aus. Oder teile uns deinen vollständigen Adresse im Text mit).",
+    )
+    return
+  }
+
+  if (session.state === State.NutzerSendetStandort) {
+    session.state = State.NutzerWartetAufTerminbestätigung
+    await reply(
+      "Vielen Dank! Wir haben deine Daten erfasst und ermitteln jetzt für dich eine medizinische Fachkraft 👨‍⚕️, um dich für den Test zu besuchen.",
+    )
     return
   }
 
   if (message?.text?.startsWith("/ichbinarzt")) {
-    reply("Schön, dass Du unterstützen möchtest!")
+    await reply("Schön, dass Du unterstützen möchtest!")
     return
   }
 
-  reply("Ich verstehe dich nicht 🤷")
+  await reply("Ich verstehe dich nicht 🤷")
 })
 
 export default async (req: NowRequest, res: NowResponse) => {
